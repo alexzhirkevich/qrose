db0ed7edb631a79e73ea6b047320e857f7595a7e
Александр Жиркевич <sasha.zhirkevich@gmail.com>
2024-03-04 01:32:27 +0300

    fix incorrect barcode encoder

--- qrose-oned/src/commonMain/kotlin/io/github/alexzhirkevich/qrose/oned/BarcodeType.kt
+++ qrose-oned/src/commonMain/kotlin/io/github/alexzhirkevich/qrose/oned/BarcodeType.kt
@@ -4,7 +4,7 @@ enum class BarcodeType(val encoder: BarcodeEncoder) {
 
     Codabar(CodabarEncoder),
     Code39(Code39Encoder),
-    Code93(Code39Encoder),
+    Code93(Code93Encoder),
     Code128(Code128Encoder),
     EAN8(CodeEAN8Encoder),
     EAN13(CodeEAN13Encoder),
